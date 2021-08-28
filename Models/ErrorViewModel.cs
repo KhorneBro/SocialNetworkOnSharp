7083370147028843cc73d248fc2d75a9d997d866
KhorneBro <dmitriiveselkov@gmail.com>
2021-08-28 14:58:23 +0600

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SocialNetworkOnSharp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
